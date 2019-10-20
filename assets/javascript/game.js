ebcbb5ec3b0279390669af2159b8b8b781daa34d
mrduncle1 <dun__can@hotmail.com>
2019-10-20 15:02:39 +0800

    final version with all aspects working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 //@ts-check
 
-let gemNumbers;
+let randNum = 0;
+let gemNumbers =[];
 let guessTotal = 0;
 let wins = 0;
 let losses = 0;
@@ -12,10 +13,8 @@ function randomNum(minimumVal, maximumVal) {
 
 function getRandNums(arrButtonNums, index) {
     let gemNum = randomNum(1, 13);
-    console.log("gemNum: " + gemNum);
     while (arrButtonNums.includes(gemNum)) {
       gemNum = randomNum(1, 13);
-      console.log("gemNum: " + gemNum);
     }
     arrButtonNums[index] = gemNum;
     return arrButtonNums;
@@ -25,49 +24,17 @@ function generateGemNos() {
     let arrButtonNums = [0, 0, 0, 0];
     $.each(arrButtonNums, function(index, value){
         getRandNums(arrButtonNums, index);
-        console.log(arrButtonNums);
     })
     return arrButtonNums;
 }
 
-function checkWin(usrTotal) {
-    if (usrTotal > $(".rando").val()) {
-        //You lose + timer
-        losses ++;
-        $("#loser").text("Losses: " + losses);
-    } 
-    else if (usrTotal === $(".rando").val()) {
-        //You win + timer
-        wins ++;
-        $("#winner").text("Wins: " + wins);
-        
-
-    })
-}
-
-function addClicks(clickID) { //clickID contains the id (gem name) of the button that was clicked
-                              //this was set in the inline onclick event contained in wach button's
-                              //html
-    console.log("Button clicked: " + clickID);
-    let gemSelect = gemNumbers.filter(function(findGem) {
-        
-        return findGem.gem === clickID;
-    })
-
-    console.log(gemSelect[0].gemNum);
-    guessTotal += (gemSelect[0].gemNum);
-    console.log(guessTotal);
-    $(".score").text(guessTotal);
-
-}
-
-$(document).ready(function() {
-    let randNum = randomNum(121, 19);
+function resetGame() {
+    $(".score").text("");
+    guessTotal = 0;
+    randNum = randomNum(121, 19);
     $(".rando").text(randNum);
-    console.log(randNum); 
 
     let buttonNumbers = generateGemNos();
-    console.log(buttonNumbers);
     gemNumbers = [
         {
            gem: "amber",
@@ -86,10 +53,59 @@ $(document).ready(function() {
            gemNum: buttonNumbers[3]
         }
     ];
-    console.log(gemNumbers);
+
+}
+
+function restartGame() {
+    if (confirm("Do you want to play again?")) {
+        gemNumbers = [];
+        $("#result").css("display", "none");
+        resetGame();
+    } else {
+        alert("Thank you for playing, please close the browser page.");
+    }
+}
+
+function checkForWin(usrTotal) {
+    console.log($(".rando").text());
+    console.log(usrTotal);
+    if (usrTotal > randNum) {
+        losses ++;
+        $("#loser").text("Losses: " + losses);
+        $("#result").css("color", "red");
+        $("#result").css("display", "block");
+        $("#result").text("You lose!!!");
+        setTimeout(function() { restartGame() }, 200);
+    } 
+    else if (usrTotal === randNum) {
+        wins ++;
+        $("#winner").text("Wins: " + wins);
+        $("#result").css("color", "green");
+        $("#result").css("display", "block");
+        $("#result").text("You win!!!");
+        setTimeout(function() { restartGame() }, 200);
+    }
+}
+
+function addClicks(clickID) { //clickID contains the id (gem name) of the button that was clicked
+                              //this was set in the inline onclick event contained in wach button's
+                              //html
+    console.log("Button clicked: " + clickID);
+    let gemSelect = gemNumbers.filter(function(findGem) {
+        return findGem.gem === clickID; //returns an anonymous object inside and array
+    })
+
+    console.log(gemSelect[0].gemNum);
+    guessTotal += (gemSelect[0].gemNum); //from the first (and only) array obtain the value associated
+                                         //with the keyword gemNum   
+    console.log(guessTotal);
+    $(".score").text(guessTotal);
+    checkForWin(guessTotal);
+    console.log(guessTotal);
+}
+
+$(document).ready(function() {
+    resetGame();
 })
 
 // $(".gem-button").click(addClicks);
-
-
-
